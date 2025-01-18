ee9aa43d8181fb0cda560b40c99ea9e1814a8355
Ikmal <ikmalkh12@gmail.com>
2025-01-18 18:03:21 +0700

    update menambahkan form Login

---
+++ project_uas/src/main/java/com/project_uas/project_uas/HelloController.java
@@ -0,0 +1,11 @@
+package com.project_uas.project_uas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    protected void onHelloButtonClick() {
+    }
+}
